d0e7fd82d1ce6f2fee4751feaa0aebd4316b2bd2
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-04-29 19:08:45 +0700

    Change matrix parse

--- РГР/src/MatrixFunctions.py
+++ РГР/src/MatrixFunctions.py
@@ -1,35 +1,61 @@
-import Fract
+from Fract import Fract
 
 def parse_matrix(matrix_str):
     rows = matrix_str.strip().split('\n')
-    left_matrix = []  
-    right_vector = []   
-    for row in rows:
-        left, right = row.strip().split('|')
-        left_parts = left.strip().split()
-        left_part = []
-        for num in left_parts:
-            if '/' in num:
-                up, low = map(int, num.split('/'))
-                left_part.append(Fract(up, low))
-            else:
-                left_part.append(Fract(int(num)))
-        right_str = right.strip()
-        if '/' in right_str:
-            up, low = map(int, right_str.split('/'))
-            right_part = Fract(up, low)
-        else:
-            right_part = Fract(int(right_str))
-        
-        left_matrix.append(left_part)
-        right_vector.append(right_part)
+    finish = False
+    matrix = []  
+    b_vector = [] 
+    z_vector = []  
+    target = 0
     
-    return left_matrix, right_vector
-
-def print_matrix(left_matrix, right_vector):
-    for i in range(len(left_matrix)):
-        row_str = ""
-        for j in range(len(left_matrix[i])):
-            row_str += f"{str(left_matrix[i][j]):>8} "
-        row_str += f"| {str(right_vector[i]):>8}"
-        print(row_str)
+    for row in rows:
+        row = row.strip()
+        if not row: 
+            continue
+            
+        if row == '_':
+            finish = True
+            continue
+            
+        if not finish:
+            if '|' not in row:
+                continue
+                
+            s_matrix, right = row.split('|')
+            left_parts = s_matrix.strip().split()
+            left_part = []
+            for num in left_parts:
+                if '/' in num:
+                    up, low = map(int, num.split('/'))
+                    left_part.append(Fract(up, low))
+                else:
+                    left_part.append(Fract(int(num)))
+            
+            right_str = right.strip()
+            if '/' in right_str:
+                up, low = map(int, right_str.split('/'))
+                right_part = Fract(up, low)
+            else:
+                right_part = Fract(int(right_str))
+        
+            matrix.append(left_part)
+            b_vector.append(right_part)
+        else:
+            if ':' not in row:
+                continue
+                
+            starget, zs = row.split(':')
+            if "max" in starget:
+                target = 1
+            elif "min" in starget:
+                target = -1
+                
+            zs_parts = zs.strip().split()
+            for num in zs_parts:
+                if '/' in num:
+                    up, low = map(int, num.split('/'))
+                    z_vector.append(Fract(up, low))
+                else:
+                    z_vector.append(Fract(int(num)))
+    
+    return matrix, b_vector, z_vector, target
